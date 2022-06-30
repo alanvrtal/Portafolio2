076257760ed0c2075abc676869f9b04f0a7dda25
alanvrtal <alanvrtal@hotmail.com>
2022-06-30 13:55:50 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MainPortafolio } from './components/MainPortafolio'
+import './assets/css/styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MainPortafolio />
+  </React.StrictMode>
+)
